d463d5d2fe6ef58c47539917e58c8e1f6bc21916
Fadi Karout <97478111+fadi-k-karout@users.noreply.github.com>
2024-02-12 13:23:34 +0000

    feat: create new Chirp
    -add the Chirp model and controller
    -modify the user model class to add a HasMany realtionship between the User and Chirp
    -modify the web routes to add a new route for the Chirps
    -add the migration for the new chirps table

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -42,4 +43,13 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Define the realtionship between the user and the chirp
+     * user HasMany chirp
+     */
+    public function chirps(): HasMany
+    {
+        return $this->hasMany(Chirp::class);
+    }
 }
